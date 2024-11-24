e628cd6ee3a591652bf8b21602bc14ba547ab1ab
Wil Simpson <wil@forever.dev>
2024-11-24 16:10:06 -0500

    feat: role array util

---
+++ pkg/util/misc.go
@@ -0,0 +1,9 @@
+package util
+
+import "github.com/WilSimpson/gocloak/v13"
+
+func RegisterRole(role *gocloak.Role, roles []*gocloak.Role) *gocloak.Role {
+	roles = append(roles, role)
+	return role
+}
+
