a832ed83a41456104adc4582fba9091b1747cf85
Wil Simpson <wil@forever.dev>
2024-11-24 14:27:20 -0500

    feat: added health server setup helper

--- pkg/srv/health.go
+++ pkg/srv/health.go
@@ -2,8 +2,14 @@ package srv
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/ShatteredRealms/go-common-service/pkg/config"
 	"github.com/ShatteredRealms/go-common-service/pkg/pb"
+	"github.com/ShatteredRealms/go-common-service/pkg/util"
+	"github.com/WilSimpson/gocloak/v13"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -18,3 +24,16 @@ func NewHealthServiceServer() pb.HealthServiceServer {
 func (s *healthService) Health(context.Context, *emptypb.Empty) (*pb.HealthMessage, error) {
 	return &pb.HealthMessage{Status: "ok"}, nil
 }
+
+func SetupHealthServer(ctx context.Context, cfg *config.BaseConfig) error {
+	keycloakClient := gocloak.NewClient(cfg.Keycloak.BaseURL)
+	grpcServer, gwmux := util.InitServerDefaults(keycloakClient, cfg.Keycloak.Realm)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	pb.RegisterHealthServiceServer(grpcServer, NewHealthServiceServer())
+	err := pb.RegisterHealthServiceHandlerFromEndpoint(ctx, gwmux, cfg.Server.Address(), opts)
+	if err != nil {
+		return fmt.Errorf("register health service handler endpoint: %w", err)
+	}
+	return nil
+}
+
