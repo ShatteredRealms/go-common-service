eaa108410636d88acf696a81fe529d9331ae6fef
Wil Simpson <wil@forever.dev>
2024-11-24 20:47:26 -0500

    feat: switched to id only targets in go

--- pkg/pb/globals.pb.go
+++ pkg/pb/globals.pb.go
@@ -20,32 +20,28 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type UserTarget struct {
+type TargetId struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	// Types that are assignable to Target:
-	//
-	//	*UserTarget_Id
-	//	*UserTarget_Username
-	Target isUserTarget_Target `protobuf_oneof:"target"`
+	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
 }
 
-func (x *UserTarget) Reset() {
-	*x = UserTarget{}
+func (x *TargetId) Reset() {
+	*x = TargetId{}
 	mi := &file_sro_globals_proto_msgTypes[0]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
 
-func (x *UserTarget) String() string {
+func (x *TargetId) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*UserTarget) ProtoMessage() {}
+func (*TargetId) ProtoMessage() {}
 
-func (x *UserTarget) ProtoReflect() protoreflect.Message {
+func (x *TargetId) ProtoReflect() protoreflect.Message {
 	mi := &file_sro_globals_proto_msgTypes[0]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -57,284 +53,17 @@ func (x *UserTarget) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use UserTarget.ProtoReflect.Descriptor instead.
-func (*UserTarget) Descriptor() ([]byte, []int) {
+// Deprecated: Use TargetId.ProtoReflect.Descriptor instead.
+func (*TargetId) Descriptor() ([]byte, []int) {
 	return file_sro_globals_proto_rawDescGZIP(), []int{0}
 }
 
-func (m *UserTarget) GetTarget() isUserTarget_Target {
-	if m != nil {
-		return m.Target
-	}
-	return nil
-}
-
-func (x *UserTarget) GetId() string {
-	if x, ok := x.GetTarget().(*UserTarget_Id); ok {
-		return x.Id
-	}
-	return ""
-}
-
-func (x *UserTarget) GetUsername() string {
-	if x, ok := x.GetTarget().(*UserTarget_Username); ok {
-		return x.Username
-	}
-	return ""
-}
-
-type isUserTarget_Target interface {
-	isUserTarget_Target()
-}
-
-type UserTarget_Id struct {
-	Id string `protobuf:"bytes,1,opt,name=id,proto3,oneof"`
-}
-
-type UserTarget_Username struct {
-	Username string `protobuf:"bytes,2,opt,name=username,proto3,oneof"`
-}
-
-func (*UserTarget_Id) isUserTarget_Target() {}
-
-func (*UserTarget_Username) isUserTarget_Target() {}
-
-type CharacterTarget struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	// Types that are assignable to Target:
-	//
-	//	*CharacterTarget_Id
-	//	*CharacterTarget_Name
-	Target isCharacterTarget_Target `protobuf_oneof:"target"`
-}
-
-func (x *CharacterTarget) Reset() {
-	*x = CharacterTarget{}
-	mi := &file_sro_globals_proto_msgTypes[1]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
-}
-
-func (x *CharacterTarget) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*CharacterTarget) ProtoMessage() {}
-
-func (x *CharacterTarget) ProtoReflect() protoreflect.Message {
-	mi := &file_sro_globals_proto_msgTypes[1]
+func (x *TargetId) GetId() string {
 	if x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use CharacterTarget.ProtoReflect.Descriptor instead.
-func (*CharacterTarget) Descriptor() ([]byte, []int) {
-	return file_sro_globals_proto_rawDescGZIP(), []int{1}
-}
-
-func (m *CharacterTarget) GetTarget() isCharacterTarget_Target {
-	if m != nil {
-		return m.Target
-	}
-	return nil
-}
-
-func (x *CharacterTarget) GetId() string {
-	if x, ok := x.GetTarget().(*CharacterTarget_Id); ok {
 		return x.Id
 	}
 	return ""
 }
-
-func (x *CharacterTarget) GetName() string {
-	if x, ok := x.GetTarget().(*CharacterTarget_Name); ok {
-		return x.Name
-	}
-	return ""
-}
-
-type isCharacterTarget_Target interface {
-	isCharacterTarget_Target()
-}
-
-type CharacterTarget_Id struct {
-	Id string `protobuf:"bytes,1,opt,name=id,proto3,oneof"`
-}
-
-type CharacterTarget_Name struct {
-	Name string `protobuf:"bytes,2,opt,name=name,proto3,oneof"`
-}
-
-func (*CharacterTarget_Id) isCharacterTarget_Target() {}
-
-func (*CharacterTarget_Name) isCharacterTarget_Target() {}
-
-type DimensionTarget struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	// Types that are assignable to Target:
-	//
-	//	*DimensionTarget_Id
-	//	*DimensionTarget_Name
-	Target isDimensionTarget_Target `protobuf_oneof:"target"`
-}
-
-func (x *DimensionTarget) Reset() {
-	*x = DimensionTarget{}
-	mi := &file_sro_globals_proto_msgTypes[2]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
-}
-
-func (x *DimensionTarget) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*DimensionTarget) ProtoMessage() {}
-
-func (x *DimensionTarget) ProtoReflect() protoreflect.Message {
-	mi := &file_sro_globals_proto_msgTypes[2]
-	if x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use DimensionTarget.ProtoReflect.Descriptor instead.
-func (*DimensionTarget) Descriptor() ([]byte, []int) {
-	return file_sro_globals_proto_rawDescGZIP(), []int{2}
-}
-
-func (m *DimensionTarget) GetTarget() isDimensionTarget_Target {
-	if m != nil {
-		return m.Target
-	}
-	return nil
-}
-
-func (x *DimensionTarget) GetId() string {
-	if x, ok := x.GetTarget().(*DimensionTarget_Id); ok {
-		return x.Id
-	}
-	return ""
-}
-
-func (x *DimensionTarget) GetName() string {
-	if x, ok := x.GetTarget().(*DimensionTarget_Name); ok {
-		return x.Name
-	}
-	return ""
-}
-
-type isDimensionTarget_Target interface {
-	isDimensionTarget_Target()
-}
-
-type DimensionTarget_Id struct {
-	Id string `protobuf:"bytes,1,opt,name=id,proto3,oneof"`
-}
-
-type DimensionTarget_Name struct {
-	Name string `protobuf:"bytes,2,opt,name=name,proto3,oneof"`
-}
-
-func (*DimensionTarget_Id) isDimensionTarget_Target() {}
-
-func (*DimensionTarget_Name) isDimensionTarget_Target() {}
-
-type MapTarget struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	// Types that are assignable to Target:
-	//
-	//	*MapTarget_Id
-	//	*MapTarget_Name
-	Target isMapTarget_Target `protobuf_oneof:"target"`
-}
-
-func (x *MapTarget) Reset() {
-	*x = MapTarget{}
-	mi := &file_sro_globals_proto_msgTypes[3]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
-}
-
-func (x *MapTarget) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*MapTarget) ProtoMessage() {}
-
-func (x *MapTarget) ProtoReflect() protoreflect.Message {
-	mi := &file_sro_globals_proto_msgTypes[3]
-	if x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use MapTarget.ProtoReflect.Descriptor instead.
-func (*MapTarget) Descriptor() ([]byte, []int) {
-	return file_sro_globals_proto_rawDescGZIP(), []int{3}
-}
-
-func (m *MapTarget) GetTarget() isMapTarget_Target {
-	if m != nil {
-		return m.Target
-	}
-	return nil
-}
-
-func (x *MapTarget) GetId() string {
-	if x, ok := x.GetTarget().(*MapTarget_Id); ok {
-		return x.Id
-	}
-	return ""
-}
-
-func (x *MapTarget) GetName() string {
-	if x, ok := x.GetTarget().(*MapTarget_Name); ok {
-		return x.Name
-	}
-	return ""
-}
-
-type isMapTarget_Target interface {
-	isMapTarget_Target()
-}
-
-type MapTarget_Id struct {
-	Id string `protobuf:"bytes,1,opt,name=id,proto3,oneof"`
-}
-
-type MapTarget_Name struct {
-	Name string `protobuf:"bytes,2,opt,name=name,proto3,oneof"`
-}
-
-func (*MapTarget_Id) isMapTarget_Target() {}
-
-func (*MapTarget_Name) isMapTarget_Target() {}
 
 type Location struct {
 	state         protoimpl.MessageState
@@ -352,7 +81,7 @@ type Location struct {
 
 func (x *Location) Reset() {
 	*x = Location{}
-	mi := &file_sro_globals_proto_msgTypes[4]
+	mi := &file_sro_globals_proto_msgTypes[1]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -364,7 +93,7 @@ func (x *Location) String() string {
 func (*Location) ProtoMessage() {}
 
 func (x *Location) ProtoReflect() protoreflect.Message {
-	mi := &file_sro_globals_proto_msgTypes[4]
+	mi := &file_sro_globals_proto_msgTypes[1]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -377,7 +106,7 @@ func (x *Location) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Location.ProtoReflect.Descriptor instead.
 func (*Location) Descriptor() ([]byte, []int) {
-	return file_sro_globals_proto_rawDescGZIP(), []int{4}
+	return file_sro_globals_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *Location) GetWorld() string {
@@ -433,37 +162,21 @@ var File_sro_globals_proto protoreflect.FileDescriptor
 
 var file_sro_globals_proto_rawDesc = []byte{
 	0x0a, 0x11, 0x73, 0x72, 0x6f, 0x2f, 0x67, 0x6c, 0x6f, 0x62, 0x61, 0x6c, 0x73, 0x2e, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x12, 0x03, 0x73, 0x72, 0x6f, 0x22, 0x46, 0x0a, 0x0a, 0x55, 0x73, 0x65, 0x72,
-	0x54, 0x61, 0x72, 0x67, 0x65, 0x74, 0x12, 0x10, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01,
-	0x28, 0x09, 0x48, 0x00, 0x52, 0x02, 0x69, 0x64, 0x12, 0x1c, 0x0a, 0x08, 0x75, 0x73, 0x65, 0x72,
-	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x08, 0x75, 0x73,
-	0x65, 0x72, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x08, 0x0a, 0x06, 0x74, 0x61, 0x72, 0x67, 0x65, 0x74,
-	0x22, 0x43, 0x0a, 0x0f, 0x43, 0x68, 0x61, 0x72, 0x61, 0x63, 0x74, 0x65, 0x72, 0x54, 0x61, 0x72,
-	0x67, 0x65, 0x74, 0x12, 0x10, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x48,
-	0x00, 0x52, 0x02, 0x69, 0x64, 0x12, 0x14, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20,
-	0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x42, 0x08, 0x0a, 0x06, 0x74,
-	0x61, 0x72, 0x67, 0x65, 0x74, 0x22, 0x43, 0x0a, 0x0f, 0x44, 0x69, 0x6d, 0x65, 0x6e, 0x73, 0x69,
-	0x6f, 0x6e, 0x54, 0x61, 0x72, 0x67, 0x65, 0x74, 0x12, 0x10, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01,
-	0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x02, 0x69, 0x64, 0x12, 0x14, 0x0a, 0x04, 0x6e, 0x61,
-	0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
-	0x42, 0x08, 0x0a, 0x06, 0x74, 0x61, 0x72, 0x67, 0x65, 0x74, 0x22, 0x3d, 0x0a, 0x09, 0x4d, 0x61,
-	0x70, 0x54, 0x61, 0x72, 0x67, 0x65, 0x74, 0x12, 0x10, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x02, 0x69, 0x64, 0x12, 0x14, 0x0a, 0x04, 0x6e, 0x61, 0x6d,
-	0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x42,
-	0x08, 0x0a, 0x06, 0x74, 0x61, 0x72, 0x67, 0x65, 0x74, 0x22, 0x86, 0x01, 0x0a, 0x08, 0x4c, 0x6f,
-	0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x14, 0x0a, 0x05, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x12, 0x0c, 0x0a, 0x01,
-	0x78, 0x18, 0x02, 0x20, 0x01, 0x28, 0x02, 0x52, 0x01, 0x78, 0x12, 0x0c, 0x0a, 0x01, 0x79, 0x18,
-	0x03, 0x20, 0x01, 0x28, 0x02, 0x52, 0x01, 0x79, 0x12, 0x0c, 0x0a, 0x01, 0x7a, 0x18, 0x04, 0x20,
-	0x01, 0x28, 0x02, 0x52, 0x01, 0x7a, 0x12, 0x12, 0x0a, 0x04, 0x72, 0x6f, 0x6c, 0x6c, 0x18, 0x05,
-	0x20, 0x01, 0x28, 0x02, 0x52, 0x04, 0x72, 0x6f, 0x6c, 0x6c, 0x12, 0x14, 0x0a, 0x05, 0x70, 0x69,
-	0x74, 0x63, 0x68, 0x18, 0x06, 0x20, 0x01, 0x28, 0x02, 0x52, 0x05, 0x70, 0x69, 0x74, 0x63, 0x68,
-	0x12, 0x10, 0x0a, 0x03, 0x79, 0x61, 0x77, 0x18, 0x07, 0x20, 0x01, 0x28, 0x02, 0x52, 0x03, 0x79,
-	0x61, 0x77, 0x42, 0x38, 0x5a, 0x36, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d,
-	0x2f, 0x53, 0x68, 0x61, 0x74, 0x74, 0x65, 0x72, 0x65, 0x64, 0x52, 0x65, 0x61, 0x6c, 0x6d, 0x73,
-	0x2f, 0x67, 0x6f, 0x2d, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2d, 0x73, 0x65, 0x72, 0x76, 0x69,
-	0x63, 0x65, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x70, 0x62, 0x3b, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x6f, 0x74, 0x6f, 0x12, 0x03, 0x73, 0x72, 0x6f, 0x22, 0x1a, 0x0a, 0x08, 0x54, 0x61, 0x72, 0x67,
+	0x65, 0x74, 0x49, 0x64, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x02, 0x69, 0x64, 0x22, 0x86, 0x01, 0x0a, 0x08, 0x4c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f,
+	0x6e, 0x12, 0x14, 0x0a, 0x05, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x05, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x12, 0x0c, 0x0a, 0x01, 0x78, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x02, 0x52, 0x01, 0x78, 0x12, 0x0c, 0x0a, 0x01, 0x79, 0x18, 0x03, 0x20, 0x01, 0x28, 0x02,
+	0x52, 0x01, 0x79, 0x12, 0x0c, 0x0a, 0x01, 0x7a, 0x18, 0x04, 0x20, 0x01, 0x28, 0x02, 0x52, 0x01,
+	0x7a, 0x12, 0x12, 0x0a, 0x04, 0x72, 0x6f, 0x6c, 0x6c, 0x18, 0x05, 0x20, 0x01, 0x28, 0x02, 0x52,
+	0x04, 0x72, 0x6f, 0x6c, 0x6c, 0x12, 0x14, 0x0a, 0x05, 0x70, 0x69, 0x74, 0x63, 0x68, 0x18, 0x06,
+	0x20, 0x01, 0x28, 0x02, 0x52, 0x05, 0x70, 0x69, 0x74, 0x63, 0x68, 0x12, 0x10, 0x0a, 0x03, 0x79,
+	0x61, 0x77, 0x18, 0x07, 0x20, 0x01, 0x28, 0x02, 0x52, 0x03, 0x79, 0x61, 0x77, 0x42, 0x38, 0x5a,
+	0x36, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x53, 0x68, 0x61, 0x74,
+	0x74, 0x65, 0x72, 0x65, 0x64, 0x52, 0x65, 0x61, 0x6c, 0x6d, 0x73, 0x2f, 0x67, 0x6f, 0x2d, 0x63,
+	0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2d, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x70, 0x6b,
+	0x67, 0x2f, 0x70, 0x62, 0x3b, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -478,13 +191,10 @@ func file_sro_globals_proto_rawDescGZIP() []byte {
 	return file_sro_globals_proto_rawDescData
 }
 
-var file_sro_globals_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_sro_globals_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_sro_globals_proto_goTypes = []any{
-	(*UserTarget)(nil),      // 0: sro.UserTarget
-	(*CharacterTarget)(nil), // 1: sro.CharacterTarget
-	(*DimensionTarget)(nil), // 2: sro.DimensionTarget
-	(*MapTarget)(nil),       // 3: sro.MapTarget
-	(*Location)(nil),        // 4: sro.Location
+	(*TargetId)(nil), // 0: sro.TargetId
+	(*Location)(nil), // 1: sro.Location
 }
 var file_sro_globals_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -499,29 +209,13 @@ func file_sro_globals_proto_init() {
 	if File_sro_globals_proto != nil {
 		return
 	}
-	file_sro_globals_proto_msgTypes[0].OneofWrappers = []any{
-		(*UserTarget_Id)(nil),
-		(*UserTarget_Username)(nil),
-	}
-	file_sro_globals_proto_msgTypes[1].OneofWrappers = []any{
-		(*CharacterTarget_Id)(nil),
-		(*CharacterTarget_Name)(nil),
-	}
-	file_sro_globals_proto_msgTypes[2].OneofWrappers = []any{
-		(*DimensionTarget_Id)(nil),
-		(*DimensionTarget_Name)(nil),
-	}
-	file_sro_globals_proto_msgTypes[3].OneofWrappers = []any{
-		(*MapTarget_Id)(nil),
-		(*MapTarget_Name)(nil),
-	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_sro_globals_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
