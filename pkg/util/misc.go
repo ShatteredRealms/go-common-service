d364be700757fa2d4ba6a22376e093c906bd217b
Wil Simpson <wil@forever.dev>
2024-11-25 18:51:50 -0500

    bug: append to array pass by value

--- pkg/util/misc.go
+++ pkg/util/misc.go
@@ -2,8 +2,8 @@ package util
 
 import "github.com/WilSimpson/gocloak/v13"
 
-func RegisterRole(role *gocloak.Role, roles []*gocloak.Role) *gocloak.Role {
-	roles = append(roles, role)
+func RegisterRole(role *gocloak.Role, roles *[]*gocloak.Role) *gocloak.Role {
+	*roles = append(*roles, role)
 	return role
 }
 
