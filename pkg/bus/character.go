592e3229dc34126a31b872039ba31091d44943fc
Wil Simpson <simpson.wil255@gmail.com>
2024-12-01 15:01:24 -0500

    feat: added owner id to character bus message

--- pkg/bus/character.go
+++ pkg/bus/character.go
@@ -8,6 +8,9 @@ type CharacterMessage struct {
 	// Id is the unique identifier of the character
 	Id string `json:"id"`
 
+	// Id is the unique identifier of the owner of the character
+	OwnerId string `json:"ownerId"`
+
 	// Deleted is a flag indicating if the character has been deleted
 	Deleted bool `json:"deleted"`
 }
