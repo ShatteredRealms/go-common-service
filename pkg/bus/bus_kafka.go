e0457a664091c1e69a08f708081b1658a069c404
Wil Simpson <wil@forever.dev>
2024-12-13 15:53:06 -0500

    feat: kafka bus to always retain

--- pkg/bus/bus_kafka.go
+++ pkg/bus/bus_kafka.go
@@ -1,10 +1,13 @@
 package bus
 
 import (
+	"context"
 	"errors"
 	"sync"
 
 	"github.com/ShatteredRealms/go-common-service/pkg/config"
+	"github.com/ShatteredRealms/go-common-service/pkg/log"
+	"github.com/segmentio/kafka-go"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -22,4 +25,37 @@ type kafkaBus[T BusMessage[any]] struct {
 
 	mu sync.Mutex
 	wg sync.WaitGroup
+}
+
+func (k *kafkaBus[T]) Close() {
+	client := kafka.Client{
+		Addr: kafka.TCP(k.brokers[0].Address()),
+	}
+	resp, err := client.AlterConfigs(context.Background(), &kafka.AlterConfigsRequest{
+		Resources: []kafka.AlterConfigRequestResource{
+			{
+				ResourceType: kafka.ResourceTypeTopic,
+				ResourceName: k.topic,
+				Configs: []kafka.AlterConfigRequestConfig{
+					{
+						Name:  "retention.ms",
+						Value: "-1",
+					},
+				},
+			},
+		},
+		ValidateOnly: false,
+	})
+
+	if err != nil {
+		log.Logger.Errorf("error updating topic %s: %v", k.topic, err)
+		return
+	}
+
+	for resource, err := range resp.Errors {
+
+		if err != nil {
+			log.Logger.Errorf("error updating topic %s: %s", resource.Name, err.Error())
+		}
+	}
 }
